f9f3f3933d47d450ef123d911ec0cbca261982a9
OlenaDidkivska <elenatyutyuma@gmail.com>
2022-08-17 23:20:16 +0300

    add vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
 
+const player = new Player('vimeo-player', {
+  id: 'vimeo-player',
+  width: 640,
+});
+
+const onPlay = function (data) {
+  console.log(data);
+};
+
+player.on('timeupdate', onPlay);
